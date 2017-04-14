13eb60f8a737b66dca82cf2512d3e9d9bbbed6c3
xg <xugan@xywy.com>
2017-04-14 10:09:29 +0800

    添加了一个自定义的progressbar

--- app/src/main/java/cn/example/com/myapplication/MainActivity.java
+++ app/src/main/java/cn/example/com/myapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package cn.example.com.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ProgressBar;
+
+import cn.example.com.myapplication.view.CBProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        CBProgressBar cbProgress = (CBProgressBar) this.findViewById(R.id.my_progress2);
+        cbProgress.setMax(100);
+        cbProgress.setProgress(100);
+        ProgressBar pb_xp = (ProgressBar) this.findViewById(R.id.pb_xp);
+        pb_xp.setMax(100);
+        pb_xp.setProgress(100);
         //测试改了部分代码，再次提交
         Log.i("MainActivity","onCreate");
     }
